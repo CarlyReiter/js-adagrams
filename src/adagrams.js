67e3e2653137d7f85fed513196d54bce3e9ef9a0
Carly Reiter <yepodacarly@gmail.com>
2018-11-18 20:01:13 -0800

    scoring functions explained and modified.

--- src/adagrams.js
+++ src/adagrams.js
@@ -33,106 +33,129 @@ const Adagrams = {
   drawLetters() {
     //The Object.entries() method returns an array of a given object's own enumerable property [key, value] pairs, in the same order as that provided by a for...in loop (the difference being that a for-in loop enumerates properties in the prototype chain as well)
 
-    //var sum = [0, 1, 2, 3].reduce(function (accumulator, currentValue) {
-//   return accumulator + currentValue;
-// }, 0);
+    //reduce ex. let sum = [0, 1, 2, 3].reduce(function (accumulator, currentValue) {
+    //   return accumulator + currentValue;
+    // }, 0);
     const bananaBag = Object.entries(Adagrams.FULLBAG).reduce((bag, [tile, count]) => {
       return bag + tile.repeat(count);
     }, '').split('');
 
+    //picking 10 of the tiles to put into the bag to play the game with
     let bag = [];
     for(let i = 0; i < 10; i++) {
-      const index = Math.floor(Math.random() * bananaBag.length);
-      bag.push(bananaBag.splice(index, 1)[0]);
+      // fancy way to choose random tile from full bananaBag
+      const randomlyChosenTile = Math.floor(Math.random() * bananaBag.length);
+      // pushes each of the chosen tile into the bag
+      bag.push(bananaBag.splice(randomlyChosenTile, 1)[0]);
     }
     // console.log(bag);
+    // unsure why the test that each item of the array is a single-letter string is failing, as when I console.log it, it appears OK
     return bag;
+
   },
 
   usesAvailableLetters(word, drawnLetters) {
+    // takes the word provided by the player and splits up the letters
     const bag = word.split('');
+
+    // takes the first of the letters from the playing "bag" and creates a copy of it.
     const copy = drawnLetters.slice(0);
 
+    // checks every one of the letters of the word to make sure they exist in the bag of playing tiles.
     return bag.every((square) => {
       const index = copy.indexOf(square);
+      // if the word has a letter that isn't in the playing bag, it returns false.
       if (index < 0 ) return false;
 
+      // otherwise, it deletes the copy that was made and returns true because the letter of the playing word did match the tile in the playing hand.
       delete copy[index];
       return true;
     });
   },
 
   SCORES: {
-    'A': 1,
-    'E': 1,
-    'I': 1,
-    'O': 1,
-    'U': 1,
-    'L': 1,
-    'N': 1,
-    'R': 1,
-    'S': 1,
-    'T': 1,
-    'D': 2,
-    'G': 2,
-    'B': 3,
-    'C': 3,
-    'M': 3,
-    'P': 3,
-    'F': 4,
-    'H': 4,
-    'V': 4,
-    'W': 4,
-    'Y': 4,
-    'K': 5,
-    'J': 8,
-    'X': 8,
-    'Q': 10,
-    'Z': 10,
+    'a': 1,
+    'e': 1,
+    'i': 1,
+    'o': 1,
+    'u': 1,
+    'l': 1,
+    'n': 1,
+    'r': 1,
+    's': 1,
+    't': 1,
+    'd': 2,
+    'g': 2,
+    'b': 3,
+    'c': 3,
+    'm': 3,
+    'p': 3,
+    'f': 4,
+    'h': 4,
+    'v': 4,
+    'w': 4,
+    'y': 4,
+    'k': 5,
+    'j': 8,
+    'x': 8,
+    'q': 10,
+    'z': 10,
+  },
+
+  scoreWord(word) {
+    // I'm unsure why there is a return from the start?!!?
+
+    return word.toLowerCase().split('').reduce((roundScore, letter) => {
+      const scorePerTile = Adagrams.SCORES[letter];
+
+      // if there isn't a score for a particular tile, that means it's not a valid choice and there will be an error
+      if(scorePerTile === undefined) {
+        throw `${letter} is a funky choice.  Not a letter!`;
+      }
+
+      // this is related to the reduce from above, adding the score of each tile to the score for the round.
+
+      return roundScore + scorePerTile;
+      // if the word is 7 or more letters long, they receive an 8-point bonus.  Not sure how or why this piece is after the function, though.
+    }, word.length < 7 ? 0 : 8);
+  },
+
+  highestScoreFrom(words) {
+    // This operates on "scored words" which are { word, score } objects
+    const comparer = (left, right) => {
+      // Select the word with best score
+      if(left.score > right.score) return left;
+      if(left.score < right.score) return right;
+
+      // Return left if they have the same length
+      if(left.word.length === right.word.length) return left;
+
+      // Return either if they have 10 letters
+      if(left.word.length === 10) return left;
+      if(right.word.length === 10) return right;
+
+      // Return the word with the fewest letters
+      if(left.word.length < right.word.length) return left;
+      return right;
+    };
+
+    
+    return words.map((word) => ({
+      word,
+      score: Adagrams.scoreWord(word),
+    })).reduce(comparer);
   }
 
 }
 
-// Adagrams.drawLetters ();
+
 export default Adagrams;
 
+// Adagrams.drawLetters ();
 
 
-
 //
-//   scoreWord(word) {
-//     return word.toUpperCase().split('').reduce((wordScore, letter) => {
-//       const letterScore = Adagrams.SCORE_CHART[letter];
-//       if(letterScore === undefined) {
-//         throw `${letter} is not in the English alphabet!`;
-//       }
 //
-//       return wordScore + letterScore;
-//     }, word.length < 7 ? 0 : Adagrams.LENGTH_BONUS);
-//   },
 //
-//   highestScoreFrom(words) {
-//     // This operates on "scored words" which are { word, score } objects
-//     const comparer = (left, right) => {
-//       // Select the word with best score
-//       if(left.score > right.score) return left;
-//       if(left.score < right.score) return right;
 //
-//       // Return left if they have the same length
-//       if(left.word.length === right.word.length) return left;
-//
-//       // Return either if they have 10 letters
-//       if(left.word.length === 10) return left;
-//       if(right.word.length === 10) return right;
-//
-//       // Return the word with the fewest letters
-//       if(left.word.length < right.word.length) return left;
-//       return right;
-//     };
-//
-//     return words.map((word) => ({
-//       word,
-//       score: Adagrams.scoreWord(word),
-//     })).reduce(comparer);
-//   },
 // };
