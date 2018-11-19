b952cc6070e25e9306a67867611bd480552a9aba
Carly Reiter <yepodacarly@gmail.com>
2018-11-19 05:29:02 -0800

    fixed the case of my letters, as that was causing the tests (particularly the game test) to fail.

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,32 +2,32 @@
 // Adagrams object is used as equivalent of a Ruby module.
 const Adagrams = {
   FULLBAG: {
-    'a': 9,
-    'b': 2,
-    'c': 2,
-    'd': 4,
-    'e': 12,
-    'f': 2,
-    'g': 3,
+    'A': 9,
+    'B': 2,
+    'C': 2,
+    'D': 4,
+    'E': 12,
+    'F': 2,
+    'G': 3,
     'h': 2,
-    'i': 9,
-    'j': 1,
-    'k': 1,
-    'l': 4,
-    'm': 2,
-    'n': 6,
-    'o': 8,
-    'p': 2,
-    'q': 1,
-    'r': 6,
-    's': 4,
-    't': 6,
-    'u': 4,
-    'v': 2,
-    'w': 2,
-    'x': 1,
-    'y': 2,
-    'z': 1
+    'I': 9,
+    'J': 1,
+    'K': 1,
+    'L': 4,
+    'M': 2,
+    'N': 6,
+    'O': 8,
+    'P': 2,
+    'Q': 1,
+    'R': 6,
+    'S': 4,
+    'T': 6,
+    'U': 4,
+    'V': 2,
+    'W': 2,
+    'X': 1,
+    'Y': 2,
+    'Z': 1
   },
 
   drawLetters() {
@@ -49,7 +49,6 @@ const Adagrams = {
       bag.push(bananaBag.splice(randomlyChosenTile, 1)[0]);
     }
     // console.log(bag);
-    // unsure why the test that each item of the array is a single-letter string is failing, as when I console.log it, it appears OK
     return bag;
 
   },
@@ -74,38 +73,38 @@ const Adagrams = {
   },
 
   SCORES: {
-    'a': 1,
-    'e': 1,
-    'i': 1,
-    'o': 1,
-    'u': 1,
-    'l': 1,
-    'n': 1,
-    'r': 1,
-    's': 1,
-    't': 1,
-    'd': 2,
-    'g': 2,
-    'b': 3,
-    'c': 3,
-    'm': 3,
-    'p': 3,
-    'f': 4,
-    'h': 4,
-    'v': 4,
-    'w': 4,
-    'y': 4,
-    'k': 5,
-    'j': 8,
-    'x': 8,
-    'q': 10,
-    'z': 10,
+    'A': 1,
+    'E': 1,
+    'I': 1,
+    'O': 1,
+    'U': 1,
+    'L': 1,
+    'N': 1,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'D': 2,
+    'G': 2,
+    'B': 3,
+    'C': 3,
+    'M': 3,
+    'P': 3,
+    'F': 4,
+    'H': 4,
+    'V': 4,
+    'W': 4,
+    'Y': 4,
+    'K': 5,
+    'J': 8,
+    'X': 8,
+    'Q': 10,
+    'Z': 10,
   },
 
   scoreWord(word) {
     // I'm unsure why there is a return from the start?!!?
 
-    return word.toLowerCase().split('').reduce((roundScore, letter) => {
+    return word.toUpperCase().split('').reduce((roundScore, letter) => {
       const scorePerTile = Adagrams.SCORES[letter];
 
       // if there isn't a score for a particular tile, that means it's not a valid choice and there will be an error
@@ -116,46 +115,42 @@ const Adagrams = {
       // this is related to the reduce from above, adding the score of each tile to the score for the round.
 
       return roundScore + scorePerTile;
-      // if the word is 7 or more letters long, they receive an 8-point bonus.  Not sure how or why this piece is after the function, though.
+      // if the word is 7 or more letters long, they receive an 8-point bonus.
+
+
+      // it's saying if the length of the word is less than 0, don't give a bonus.  If it's 7 or more, give the bonus.  Not sure how or why this ternary is after the function, though.  And not sure how it adds the bonus to the score.
     }, word.length < 7 ? 0 : 8);
   },
 
   highestScoreFrom(words) {
     // This operates on "scored words" which are { word, score } objects
-    const comparer = (left, right) => {
-      // Select the word with best score
-      if(left.score > right.score) return left;
-      if(left.score < right.score) return right;
+    const whichWins = (optionA, optionB) => {
 
-      // Return left if they have the same length
-      if(left.word.length === right.word.length) return left;
+      // Figuring out which of the options has a larger score
+      if(optionA.score > optionB.score) return optionA;
+      if(optionA.score < optionB.score) return optionB;
 
-      // Return either if they have 10 letters
-      if(left.word.length === 10) return left;
-      if(right.word.length === 10) return right;
+      // If both scores are equal, and the lengths are equal, OptionA wins
+      if(optionA.word.length === optionB.word.length) return optionA;
 
-      // Return the word with the fewest letters
-      if(left.word.length < right.word.length) return left;
-      return right;
+      // If one of the words used all 10 of the tiles, return that one.
+      if(optionA.word.length === 10) return optionA;
+      if(optionB.word.length === 10) return optionB;
+
+      // If none of the above selectors worked, use this option to figure out which one one.
+      if(optionA.word.length < optionB.word.length) return optionA;
+      return optionB;
     };
 
-    
+    // Whichever won on the above function, return that word and the score as the winner.
     return words.map((word) => ({
       word,
       score: Adagrams.scoreWord(word),
-    })).reduce(comparer);
+    })).reduce(whichWins);
   }
 
 }
 
-
 export default Adagrams;
 
 // Adagrams.drawLetters ();
-
-
-//
-//
-//
-//
-// };
